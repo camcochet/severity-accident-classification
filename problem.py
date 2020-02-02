4ea51852e3e50b2e443c802d63571231c1ec8b96
camcochet <44912186+camcochet@users.noreply.github.com>
2020-02-02 22:39:04 +0100

    Add files via upload

--- problem.py
+++ problem.py
@@ -2,53 +2,62 @@ import os
 import pandas as pd
 import rampwf as rw
 from rampwf.workflows import FeatureExtractorRegressor
+from rampwf.workflows import FeatureExtractorClassifier
 from rampwf.score_types.base import BaseScoreType
 from sklearn.model_selection import GroupShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from rampwf.score_types.classifier_base import ClassifierBaseScoreType
+import numpy as np
 
-problem_title = 'Prediction of the gravity of an accident'
+problem_title = 'Prediction of the severity of an accident'
 _target_column_name = 'grav' 
 # A type (class) which will be used to create wrapper objects for y_pred
-Predictions = rw.prediction_types.make_regression()
+_prediction_label_names = [1, 2, 3, 4]
+Predictions = rw.prediction_types.make_multiclass(
+    label_names=_prediction_label_names)
+#Predictions = rw.prediction_types.make_regression()
 # An object implementing the workflow
 
-
-
-class FAN(FeatureExtractorRegressor):
+class Accident(FeatureExtractorClassifier):
     def __init__(self, workflow_element_names=[
-            'feature_extractor', 'regressor']):
-        super(FAN, self).__init__(workflow_element_names[:2])
+            'feature_extractor', 'classifier']):
+        super(Accident, self).__init__(workflow_element_names[:2])
         self.element_names = workflow_element_names
 
-workflow = FAN()
+workflow = Accident()
 
-# define the score (specific score for the FAN problem)
-class FAN_error(BaseScoreType):
+class Accident_error(ClassifierBaseScoreType):
     is_lower_the_better = False
     minimum = 0.0
     maximum = float('inf')
 
-    def __init__(self, name='fan error', precision=2):
+    def __init__(self, name='error', precision=2):
         self.name = name
         self.precision = precision
 
     def __call__(self, y_true, y_pred):
+
         if isinstance(y_true, pd.Series):
             y_true = y_true.values
 
-        balanced_score = balanced_accuracy_score(y_true, y_pred.round())
+        balanced_score = balanced_accuracy_score(y_true, y_pred)
         
         return balanced_score
 
 score_types = [
-    FAN_error(name='Accuracy error', precision=2),
+    Accident_error(name='error', precision=2),
 ]
 
+#score_types = [
+#rw.score_types.BalancedAccuracy(name='BalancedAccuracy', precision=3)
+#]
+
 def get_cv(X, y):
-    cv = GroupShuffleSplit(n_splits=8, test_size=0.20, random_state=42)
-    return cv.split(X,y, groups=X.index)
+    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
+    return cv.split(X, y)
 
 def _read_data(path, f_name1,f_name2,f_name3,f_name4):
     data1 = pd.read_csv(os.path.join(path, 'data', f_name1), low_memory=False)
@@ -58,9 +67,7 @@ def _read_data(path, f_name1,f_name2,f_name3,f_name4):
     dataMerge = pd.merge(data1,data2, on='Num_Acc', how='left')
     dataMerge1 =  pd.merge(dataMerge,data3, on='Num_Acc', how='left')
     dataMerge2 = pd.merge(dataMerge1,data4, on='Num_Acc', how='left')
-    encoder = LabelEncoder()
     y_array = dataMerge2[_target_column_name].values
-    y_array = encoder.fit_transform(y_array)
     X_df = dataMerge2.drop(_target_column_name, axis=1)
     return X_df, y_array
 
